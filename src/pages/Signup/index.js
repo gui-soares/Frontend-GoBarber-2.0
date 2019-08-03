6c8f24d83202792cc12ada410be8351fbb55d970
gui-soares <eng.guilhermemsoares@gmail.com>
2019-08-03 00:17:15 -0300

    Estilização de autenticação

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-export default function Signup() {
-  return <div />;
+export default function Singup() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
